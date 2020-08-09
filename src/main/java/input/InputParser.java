c3da886a608b17f7426d96554ad8f7e6335157a2
dhor051 <dhor051@aucklanduni.ac.nz>
2020-08-09 19:30:46 +1200

    made input parsing more robust to handle cases such as longer vertex id. and longer weights

--- src/main/java/input/InputParser.java
+++ src/main/java/input/InputParser.java
@@ -43,11 +43,7 @@ public class InputParser {
 
             while ((line = bufferReader.readLine()) != null) {
 
-        System.out.println(line);
-
-        if (line.substring(0, 1).equals("}")) {
-        break;
-        }
+                System.out.println(line);
 
                 // end of file
                 if (line.substring(0, 1).equals("}")) {
@@ -55,100 +51,114 @@ public class InputParser {
                 }
 
 
-        if (line.contains(">")) { // edges
+
+                line = line.trim();
+                line = line.replaceAll("\\s+","");
+
+                if (line.contains(">")) { // edges
+
+                    //split the line into child and parent vertex id and edge weight
+                    // in form: child and parent vertex id , edge weight
+                    String[] lineSplitEdgeAndWeight = line.split("\\[Weight=");
+
+                    String childAndParentVertexID = lineSplitEdgeAndWeight[0];
+                    String edgeWeight = lineSplitEdgeAndWeight[1];
+
+                    //split the childAndParentVertexID into child and parent vertex id
+                    // in form: child vertex id,  parent vertex id
+                    String[] childAndParentVertexIDSplit = childAndParentVertexID.split(">");
+
+                    // find vertex id of child and parent
+                    String parentVertexID = childAndParentVertexIDSplit[0];
+                    parentVertexID = parentVertexID.replaceAll("[^A-Za-z0-9 -]", "");
+                    String childVertexID = childAndParentVertexIDSplit[1];
+                    childVertexID = childVertexID.replaceAll("[^A-Za-z0-9 -]", "");
+
+                    // find edge weight
+                    edgeWeight = edgeWeight.replaceAll("[^-?0-9]+", "");
+                    int edgeWeightInt = Integer.parseInt(edgeWeight);
+
+                    // create edge id
+                    String edgeId = parentVertexID+"->"+childVertexID;
 
 
-            //change this so that it is not hardcoded.
-        char parentChar =  line.charAt(0);
-        char childChar = line.charAt(5);
+                    // Find parent and child vertices for edge
+                    HashMap<String, Vertex> hashVertices = algoGraph.getHashVertices();
+                    Vertex parent = hashVertices.get(parentVertexID);
+                    Vertex child = hashVertices.get(childVertexID);
+
+                    //instantiate edge
+                    Edge edge = new Edge(parent, child, edgeWeightInt);
+
+                    // add edge to graph data.
+                    algoGraph.addEdge(edgeId, edge);
 
 
-            String parentNode = Character.toString(parentChar);
-            String childNode = Character.toString(childChar);
-
-            int weight = Integer.parseInt(Character.toString(line.charAt(15)));
-            String edgeId = parentNode+"->"+childNode;
+                    edgeCount++;
 
 
-            System.out.println("edgeId ="+edgeId);
-            System.out.println("edgeWeight ="+weight);
+                    // adding incoming edges and outgoing edges for each vertex for faster searching
+                    parent.addOutgoingEdge(edge);
+                    child.addIncomingEdge(edge);
+
+                    // adding incoming vertices and outgoing vertices for each vertex for faster searching
+                    parent.addOutgoingVertex(edge.getEndVertex());
+                    child.addIncomingVertex(edge.getStartVertex());
 
 
-            HashMap<String, Vertex> hashVertices = algoGraph.getHashVertices();
-
-            Vertex parent = hashVertices.get(parentNode);
-
-            Vertex child = hashVertices.get(childNode);
-            Edge edge = new Edge(parent, child, weight);
-
-            System.out.println(edge);
-            System.out.println(parent);
-
-            algoGraph.addEdge(edgeId, edge);
-
-            edgeCount++;
-
-            parent.addOutgoingEdge(edge);
-            child.addIncomingEdge(edge);
-
-
-            parent.addOutgoingVertex(edge.getEndVertex());
-            child.addIncomingVertex(edge.getStartVertex());
+                } else if (!line.contains(">")) { // nodes
 
 
 
-        } else if (!line.contains("->")) { // nodes
+                    //split the line into vertex id and vertex weight
+                    // in form: id , weight
+                    String[] lineSplit = line.split("\\[Weight=");
+
+                    // find vertex id
+                    String vertexID = lineSplit[0];
+                    vertexID = vertexID.replaceAll("[^A-Za-z0-9 -]", "");
 
 
-            //may move these outside the if statements to use with vertex
-            line = line.trim();
-            line.replaceAll( "\\s+", "");
+                    // find vertex weight
+                    String vertexWeight = lineSplit[1];
+                    vertexWeight = vertexWeight.replaceAll("[^-?0-9]+", "");
+                    int vertexWeightInt = Integer.parseInt(vertexWeight);
 
 
-
-            String vertexID = line.substring(0, 1);
-            String vertexWeight = line.split("\\[Weight=")[1];
-            vertexWeight = vertexWeight.replaceAll("[^-?0-9]+", "");
-            int vertexWeightInt = Integer.parseInt(vertexWeight);
-
-            System.out.println(vertexID);
-            System.out.println(vertexWeight);
-
-            Vertex graphVertex = new Vertex(vertexID, vertexWeightInt);
-
-            System.out.println(graphVertex);
-
-            algoGraph.addVertex(vertexID, graphVertex);
+                    // Instantiate vertex using ID and weight
+                    // and then add to the graph.
+                    Vertex graphVertex = new Vertex(vertexID, vertexWeightInt);
+                    algoGraph.addVertex(vertexID, graphVertex);
 
 
-        } else { //end of file
-        System.out.println("end of file or GG");
+                } else { //end of file
+                    System.out.println("end of file or GG");
 
-        }
+                }
 
 
-        }
+            }
 
             // checking algo
-/*           System.out.println(algoGraph.getHashVertices());
+           System.out.println(algoGraph.getHashVertices());
             System.out.println(algoGraph.getHashEdges());
             Vertex vertexB = algoGraph.getHashVertices().get("b");
             System.out.println("incomming edges = " + vertexB.getIncomingEdges());
-            System.out.println("OutgoingVertices = " + vertexB.getOutgoingVertices());*/
+            System.out.println("OutgoingVertices = " + vertexB.getOutgoingVertices());
 
         } catch (IOException e) {
-        e.printStackTrace();
+            e.printStackTrace();
         } finally {
-        try {
-        bufferReader.close();
-        } catch (IOException e) {
-        e.printStackTrace();
-        }
+            try {
+                bufferReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
 
-        }
-        }
+    }
+}
 
 
 
