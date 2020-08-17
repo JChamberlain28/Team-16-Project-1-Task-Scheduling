e5170235ed26ce69b38dcd69820eaa6a2723ee72
dhor051 <dhor051@aucklanduni.ac.nz>
2020-08-17 13:44:29 +1200

    3 compulsary options added

--- src/main/java/input/CliParser.java
+++ src/main/java/input/CliParser.java
@@ -9,7 +9,7 @@ import java.net.URISyntaxException;
 import java.security.CodeSource;
 
 
-//import org.apache.commons.cli.*;
+import org.apache.commons.cli.*;
 
 
 public class CliParser {
@@ -18,18 +18,13 @@ public class CliParser {
     private static final CliParser CliParsedInputs = new CliParser();
     private String filePathName;
     private int numberOfProcessors;
+    private int numberOfCores;
     private String outputFileName;
     private boolean visualisationDisplay;
 
 
 
-    private CliParser(){
-    }
-
-
-
-
-    public static void UI(String[] args) {
+    public void UI(String[] args) {
         if (args.length == 0) {
             System.err.println("No arguments provided");
         }
@@ -41,15 +36,15 @@ public class CliParser {
         }
     }
 
-    private static void handleInput(String[] args) {
+    private void handleInput(String[] args) {
 
         // check for all the strings validity
-        String[] result = parseCli(args); // result[0]: file path, result[1]: num. processors, result[2]: output file, result[3]: num. cores, result[4]: visualize
+        parseCli(args);
 
-        if (result != null) {
+        /*if (result != null) {
             try {
 
-/*                //main method here.
+              //main method here.
                 // get directory of jar
                 // wtf?
                 CodeSource codeSource = CliParser.class.getProtectionDomain().getCodeSource();
@@ -84,69 +79,186 @@ public class CliParser {
 
                 //option to add output fie name
                 // need a method to create Output File Name.
-               //OutputGenerator.generate(graph, result[2], jarDir);*/
+               //OutputGenerator.generate(graph, result[2], jarDir);
 
 
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }
+        }*/
     }
 
 
 
 
-    private static String[] parseCli(String[] args) {
+    private void parseCli(String[] args) {
 
 
-
-
-
-        String[] result = new String[5];
 
         // method to make new output file name should be added
+        CliParsedInputs.numberOfCores = 1;  //default number of cores
         CliParsedInputs.outputFileName = "output.dot"; //default output name
         CliParsedInputs.visualisationDisplay = false;  // default visualisation boolean (true or false)
 
-        // Mandatory options
-        if (args.length > 1) { // If both file path and number of processors are entered
-            if (args[0].endsWith(".dot")) {
+
+        // input format is such that first two args are given without options
+        if (args.length > 1) { // both file path and number of processors
+            if (checkValidFileName(args[0])) {
                 CliParsedInputs.filePathName = args[0]; // File path
             } else {
                 //throw exception here?
                 System.err.println("Invalid file path name");
-                result = null;
+                //throw new IllegalArgumentException("Invalid file path name");
             }
 
-
-            if (result != null){
-                // number of processors
-                if ( checkValidStringInt(args[1])) {
-                    CliParsedInputs.numberOfProcessors = Integer.parseInt(args[1]);
-                } else {
-                    System.err.println("Invalid number of processors");
-                }
-
-
-
-                // visualisation
-                if (checkValidBoolean(args[4])){
-                    CliParsedInputs.visualisationDisplay = Boolean.parseBoolean(args[4]);
-                }  else {
-                    System.err.println("Invalid visualisation parameter");
-                }
-
+            // number of processors
+            if ( checkValidStringInt(args[1])) {
+                CliParsedInputs.numberOfProcessors = Integer.parseInt(args[1]);
             } else {
-                // null result due to invalid input file name
+                System.err.println("Invalid number of processors");
+                //throw new IllegalArgumentException("Invalid number of processors");
             }
+
+
+
+
         } else {
             // does not have minimum number of args.
             System.err.println("0 arguments");
+            //throw new IllegalArgumentException("0 arguments");
         }
 
-        return result;
+
+
+        try {
+
+            // detects all parsed arg options
+            CommandLine commandLineParsed = parseOptionArguments(args);
+
+            // option for parallisation with n number of cores
+            if (commandLineParsed.hasOption("p")) {
+                String numberOfCoresInput = commandLineParsed.getOptionValue("p");
+                if (checkValidStringInt(numberOfCoresInput)) {
+                    CliParsedInputs.numberOfCores = Integer.parseInt(numberOfCoresInput);
+                }else{
+
+                    //throw new IllegalArgumentException("invalid number of cores");
+                }
+            }
+
+            // option for visualisation
+            if (commandLineParsed.hasOption("v")) {
+                String visualisationDisplayInput = commandLineParsed.getOptionValue("v");
+                if (checkValidBoolean(visualisationDisplayInput )){
+                    CliParsedInputs.visualisationDisplay = Boolean.parseBoolean(visualisationDisplayInput);
+                } else{
+                    //throw new IllegalArgumentException("invalid input for visualisation");
+                }
+            }
+
+            // option for choosing an output file name
+            if (commandLineParsed.hasOption("o")) {
+                String outputFileNameInput = commandLineParsed.getOptionValue("o");
+                if (checkValidFileName(outputFileNameInput)) {
+                    CliParsedInputs.outputFileName = outputFileNameInput;
+                }
+            }
+
+
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
+
+
+    public static boolean checkValidBoolean(String booleanString){
+        return(booleanString.equalsIgnoreCase("true") || booleanString.equalsIgnoreCase("false"));
+    }
+
+    public static boolean checkValidFileName(String fileName){
+        return (fileName.endsWith(".dot"));
+    }
+
+
+
+    public static boolean checkValidStringInt(String intString) {
+        try {
+            if (Integer.parseInt(intString) >= 1){
+                return true;
+            } else {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+    }
+
+
+    private CommandLine parseOptionArguments(String[] optionArguments) {
+        Options cliOptions = createOptions();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine line=null; //might change
+        try {
+            line = parser.parse(cliOptions, optionArguments); // args are the arguments passed to the  the application via the main method
+            if (line.hasOption("output")){
+                //do something
+            } else if(line.hasOption("name")){
+                // do something else
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        // too many args
+        if (line.getArgList().size() > 2){
+            throw new IllegalArgumentException("too many arguments");
+        }
+        return line;
+    }
+
+
+
+/*
+ options to be included
+−p N use N cores for execution in parallel (default is sequential )
+−v visualise the search
+−o OUPUT output file is named OUTPUT ( default is INPUT−output.dot)*/
+
+    private Options createOptions(){
+
+        Options createdOptions = createOptions();
+
+        Option numberOfCoresOption =    OptionBuilder.hasArgs(1)
+                .withArgName("number of cores")
+                .withDescription("Number of cores for execution in parallel")
+                .create("p");
+
+        Option visualisationOption = OptionBuilder.hasArgs(1)
+                .withArgName("search visualisation")
+                .withDescription("Visualisation of the search")
+                .create("v");
+
+        Option outputOption = OptionBuilder.hasArgs(1)
+                .withArgName("output file name")
+                .withDescription("The output file name")
+                .create("o");
+
+
+        createdOptions.addOption(numberOfCoresOption);
+        createdOptions.addOption(visualisationOption);
+        createdOptions.addOption(outputOption);
+
+
+        return createdOptions;
+
+    }
+
+
 
     // get object with parsed inputs
     public static CliParser getCliParserInstance(){
@@ -170,87 +282,9 @@ public class CliParser {
         return visualisationDisplay;
     }
 
-
-
-    public static boolean checkValidBoolean(String booleanString){
-
-        return(booleanString.equalsIgnoreCase("true") || booleanString.equalsIgnoreCase("false"));
-
+    public int getNumberOfCores(){
+        return numberOfCores;
     }
-
-
-    public static boolean checkValidStringInt(String intString) {
-        try {
-            if (Integer.parseInt(intString) >= 1){
-                return true;
-            } else {
-                return false;
-            }
-        } catch (NumberFormatException e) {
-            return false;
-        }
-
-    }
-
-
-
-
-
-
-
-
-
-    /*
-    *
-    *
-    *
-    *
-    *
-    *
-    * https://stackoverflow.com/questions/11704338/java-cli-commandlineparser
-    * ...
-Option opt1 = OptionBuilder.hasArgs(1).withArgName("output directory")
-    .withDescription("This is the output directory").isRequired(true)
-    .withLongOpt("output").create("O");
-
-Option opt2 = OptionBuilder.hasArgs(1).withArgName("file name")
-    .withDescription("This is the file name").isRequired(true)
-    .withLongOpt("name").create("N")
-
-Options o = new Options();
-o.addOption(opt1);
-o.addOption(opt2);
-CommandLineParser parser = new BasicParser();
-
-try {
-  CommandLine line = parser.parse(o, args); // args are the arguments passed to the  the application via the main method
-  if (line.hasOption("output") {
-     //do something
-  } else if(line.hasOption("name") {
-     // do something else
-  }
-} catch(Exception e) {
-  e.printStackTrace();
-}
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    * */
-
-
-
-
-
-
-
-
-
-
-
 
 
 
