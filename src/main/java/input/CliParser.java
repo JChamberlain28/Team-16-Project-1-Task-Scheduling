2ae11bd3fbd7a87c44a108983c36518ff2dba52f
dhors <smexylure@gmail.com>
2020-08-30 10:42:29 +1200

    removed check for number of cores as it is actually number of threads

--- src/main/java/input/CliParser.java
+++ src/main/java/input/CliParser.java
@@ -186,15 +186,18 @@ public class CliParser {
             // Checking valid number of cores.
             if (numberOfCoresInput != null && checkValidStringInt(numberOfCoresInput)) {
                 int numberOfCoresInt = Integer.parseInt(numberOfCoresInput);
-                if (Runtime.getRuntime().availableProcessors() > numberOfCoresInt) {
-                    System.out.println("num of cores available - " + Runtime.getRuntime().availableProcessors());
+               /* if (Runtime.getRuntime().availableProcessors() > numberOfCoresInt) */
+                if (Runtime.getRuntime().availableProcessors() > numberOfCoresInt){
+                    System.out.println("There are " +Runtime.getRuntime().availableProcessors()+ " available processors.");
+                    System.out.println(numberOfCoresInt + " Threads will be created");
+                }
                     _CliParsedInputs._numberOfCores = Integer.parseInt(numberOfCoresInput);
-                } else {
+               /* } else {
                     // does not have this many cores
                     throw new IllegalArgumentException("Error: Too large for number of cores inputted." +
                             " Please enter a valid number of cores. There are " + Runtime.getRuntime().availableProcessors()
                             + " cores available");
-                }
+                }*/
             } else {
                 throw new IllegalArgumentException("Error: invalid number of cores." +
                         " Please enter a valid number of cores.");
